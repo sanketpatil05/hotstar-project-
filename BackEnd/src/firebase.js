93b0bd0fbd005f1f5a047dae12bbd75571aa0967
Aashish Kumar <45985735+AASHISHKUMAR321@users.noreply.github.com>
2022-04-03 06:59:20 +0530

    Add files via upload

---
+++ BackEnd/src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+var firebaseConfig = {
+  apiKey: "",
+  authDomain: "", 
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+export default firebase
